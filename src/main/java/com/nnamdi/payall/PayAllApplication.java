a9a325203fc14ae66c37c151f2d3a81a1ce30c7d
nnamdi16 <nwabuokeinnamdi@gmail.com>
2023-10-24 21:49:49 +0200

    initial commit

---
+++ src/main/java/com/nnamdi/payall/PayAllApplication.java
@@ -0,0 +1,13 @@
+package com.nnamdi.payall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayAllApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayAllApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nnamdi/payall/PayAllApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nnamdi.payall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayAllApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
