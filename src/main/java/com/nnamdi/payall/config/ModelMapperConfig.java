8d53596950a6f30901e71e4e4ff9eadc02fd49ca
nnamdi16 <nwabuokeinnamdi@gmail.com>
2023-10-26 22:13:04 +0200

    Added ModelMapperConfig

---
+++ src/main/java/com/nnamdi/payall/config/ModelMapperConfig.java
@@ -0,0 +1,17 @@
+package com.nnamdi.payall.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setFieldMatchingEnabled(true)
+                .setFieldAccessLevel(org.modelmapper.config.Configuration.AccessLevel.PRIVATE)
+                .setAmbiguityIgnored(true);
+        return  modelMapper;
+    }
+}
